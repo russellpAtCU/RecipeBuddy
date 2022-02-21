599cc3e489251a412c061607e8e0fdcb4883dc7a
RanksDG <53052392+RanksDG@users.noreply.github.com>
2022-02-21 16:59:33 -0500

    Shell of Recipe Creation and Recipe display page

---
+++ rbp.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+   
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "View All";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "View less";
+      moreText.style.display = "inline";
+    }
+  }
+  
